f642f1486266646e9c73c6fb0421c286903639d4
Kazuya Iimuro <ikazuya0201@gmail.com>
2020-08-17 00:25:33 +0900

    [add] Implemented distance sensor.

--- src/sensors.rs
+++ src/sensors.rs
@@ -1,9 +1,11 @@
 mod encoder;
 mod imu;
 mod motor;
+mod tof;
 mod voltmeter;
 
 pub use encoder::{EncoderError, MA702GQ};
 pub use imu::{IMUError, ICM20648};
 pub use motor::{IMotor, Voltmeter};
+pub use tof::VL6180X;
 pub use voltmeter::IVoltmeter;
